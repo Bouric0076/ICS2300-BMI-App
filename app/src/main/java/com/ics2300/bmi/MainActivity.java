a515a32471197d96a2cf8d13835be210608a814b
Bouric Enos <bouricenos0@gmail.com>
2026-01-12 19:24:27 +0300

    Initial project skeleton with basic UI and activity structure

---
+++ app/src/main/java/com/ics2300/bmi/MainActivity.java
@@ -0,0 +1,30 @@
+package com.ics2300.bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etWeight, etHeight;
+    Button btnCalculate;
+    TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etWeight = findViewById(R.id.etWeight);
+        etHeight = findViewById(R.id.etHeight);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        tvResult = findViewById(R.id.tvResult);
+
+        // TODO: Add button click listener
+        // TODO: Add input validation
+        // TODO: Add BMI calculation logic
+        // TODO: Display result and classification
+    }
+}
